e8b51bdcd38c341d4e5fe00b201659142e0a0714
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2023-10-11 16:48:51 +0200

    Implementation de gestion des inputProcessors

--- src/core/src/com/dungeondevs/dungeongame/Jeu.java
+++ src/core/src/com/dungeondevs/dungeongame/Jeu.java
@@ -1,6 +1,7 @@
 package com.dungeondevs.dungeongame;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import screens.WorldScreen;
 
 public class Jeu extends Game {
@@ -8,7 +9,9 @@ public class Jeu extends Game {
     @Override
     public void create() {
 //        dungeon = new Dungeon();
-        setScreen(new WorldScreen());
+        WorldScreen ws = new WorldScreen();
+        setScreen(ws);
+        Gdx.input.setInputProcessor(ws.getInputProcessor());
     }
 
     @Override
