527cc0b9665341cb8ce6fecb385e7f61e8d68b10
yoan-ngr <yoan.nougueruiz@gmail.com>
2023-11-22 15:02:46 +0100

    Affichage bonus vitesse sur le HUD

--- src/core/src/com/dungeondevs/systems/HudSystem.java
+++ src/core/src/com/dungeondevs/systems/HudSystem.java
@@ -9,19 +9,21 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.StringBuilder;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.dungeondevs.components.AttackComponent;
-import com.dungeondevs.components.HealthComponent;
-import com.dungeondevs.components.PlayerCharacterComponent;
+import com.dungeondevs.components.*;
 
 @All(PlayerCharacterComponent.class)
 public class HudSystem extends BaseEntitySystem {
     private Stage hudStage;
     private Label healthLabel;
     private Label weaponLabel;
+    private Label powerUpLabel;
     private ComponentMapper<HealthComponent> healthMapper;
     private ComponentMapper<AttackComponent> weaponMapper;
+    private ComponentMapper<PowerUpUserComponent> powerUpMapper;
 
     @Override
     protected void initialize() {
@@ -30,14 +32,23 @@ public class HudSystem extends BaseEntitySystem {
 
         // Create the Label that will display the health
         Label.LabelStyle labelStyle = new Label.LabelStyle(new BitmapFont(),new Color(1,1,1,1));
+
         healthLabel = new Label("Health: ", labelStyle);
         weaponLabel = new Label("Weapon : ", labelStyle);
+        powerUpLabel = new Label("", labelStyle);
+
         Table table = new Table(); // Create the layout table
         table.setFillParent(true);
         hudStage.addActor(table);
         table.add(healthLabel).padTop(10).padRight(10);
         table.add(weaponLabel).padTop(10).padRight(10);
         table.top().right();
+
+        Table powerUpTable = new Table();
+        powerUpTable.setFillParent(true);
+        hudStage.addActor(powerUpTable);
+        powerUpTable.add(powerUpLabel);
+        powerUpTable.top().left();
     }
 
     @Override
@@ -46,8 +57,10 @@ public class HudSystem extends BaseEntitySystem {
         if (!entities.isEmpty()) {
             HealthComponent healthComponent = healthMapper.get(entities.get(0));
             AttackComponent attackComponent = weaponMapper.get(entities.get(0));
+            PowerUpUserComponent powerUpUserComponent = powerUpMapper.get(entities.get(0));
             updateHealthLabel(healthComponent);
             updateWeaponLabel(attackComponent);
+            updatePowerupLabel(powerUpUserComponent);
             hudStage.act(world.delta);
             hudStage.draw();
         }
@@ -59,6 +72,15 @@ public class HudSystem extends BaseEntitySystem {
 
     private void updateWeaponLabel(AttackComponent attackComponent) {
         weaponLabel.setText("Arme actuelle: " + attackComponent.arme);
+    }
+
+    private void updatePowerupLabel(PowerUpUserComponent powerUpUserComponent) {
+        boolean speedPowerUpActive = powerUpUserComponent.speedLastAppliedTime > 0;
+        float remainingSpeedTime = (float) (powerUpUserComponent.speedLastAppliedTime + powerUpUserComponent.speedDuration - TimeUtils.millis()) / 1000L;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(speedPowerUpActive ? "Bonus de vitesse : " + String.format("%.1f", remainingSpeedTime) : "");
+        powerUpLabel.setText(stringBuilder.toString());
     }
 
     @Override
